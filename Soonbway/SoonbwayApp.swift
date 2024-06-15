a2cdbf91cb4e2aa9559aeafb6abd32a24c81bce8
ParkHoHo <hohoho00@sch.ac.kr>
2024-06-15 15:30:32 +0900

    Initial Commit

---
+++ Soonbway/SoonbwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoonbwayApp.swift
+//  Soonbway
+//
+//  Created by ParkHoHo on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SoonbwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
